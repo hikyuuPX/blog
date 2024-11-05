773b4dc0f612a3f74e12c1a9de2e41572ac76a1a
hwchu <hwchu@zdmedtech.com>
2024-11-05 15:21:52 +0800

    first commit.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+const beian = 'CC BY-NC 4.0'
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
@@ -7,16 +8,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'hikyuuPX的随笔',
-  tagline: '【道常无为而无不为】',
   url: 'https://your-docusaurus-site.example.com',
   baseUrl: '/',
   favicon: 'img/favicon.jpg',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'hikyuuPX', // Usually your GitHub org/user name.
-  projectName: 'blog', // Usually your repo name.
-  onBrokenLinks: 'ignore',
+  tagline: '【道常无为而无不为】',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -50,19 +45,32 @@ const config = {
       }),
     ],
   ],
+
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   themeConfig: {
     image: 'img/logo.png',
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
     navbar: {
       logo: {
         alt: 'hikyuuPX的随笔',
         src: 'img/favicon.webp',
       },
+      hideOnScroll: true,
       items: [ //顶部菜单栏
         {
           label: '博客',
           position: 'right',
-          to: "docs/Blogs/",
+          items: [{
+            label: 'C语言',
+            to: 'docs/Blogs/C',
+          }, {
+            label: 'Linux',
+            to: 'docs/Blogs/linux',
+          }, ],
         }, {
           label: '笔记',
           position: 'right',
@@ -80,13 +88,8 @@ const config = {
     },
     footer: { //页面底部菜单栏
       style: 'dark',
-      copyright: `本站所有内容遵循<a href="https://creativecommons.org/licenses/by/4.0/deed.zh" ></a>开源协议，仅限非商业性使用，转载请注明出处
-                    <p>Copyright © ${new Date().getFullYear()} - PRESENT hikyuuPX, Inc. Built with Docusaurus.</p>
-                    <script type="text/javascript">function show_runtime(){window.setTimeout("show_runtime()",1000);X=new 
-                    Date("11/05/2024 12:00:00");
-                    Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;
-                    a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);
-                    runtime_span.innerHTML="<br>本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();</script>`,
+      copyright: `本站所有内容遵循<a href="https://creativecommons.org/licenses/by/4.0/deed.zh" >${beian}</a>开源协议，仅限非商业性使用，转载请注明出处</p>
+                    <p>Copyright © ${new Date().getFullYear()} - PRESENT hikyuuPX, Inc. Built with Docusaurus.</p>`,
     },
     prism: {
       theme: lightCodeTheme,
