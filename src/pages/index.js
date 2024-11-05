a9df9caa3fe036865d5f9f7c9d24a6dcf85a44b3
hwchu <hwchu@zdmedtech.com>
2024-11-05 14:51:53 +0800

    first commit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,119 +1,203 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from 'react'
+import { Variants, motion, useScroll, useTransform } from 'framer-motion' // Import motion from framer-motion
 
-import Head from '@docusaurus/Head'
-import HeroImg from '../../static/img/HomePage.png'
+import Translate from '@docusaurus/Translate'
 
-import styles from './index.module.css';
+import HeroMain from './img/hero_main.svg'
 
-/*原本的HomepageHeader函数文件*/ 
-/*function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+import styles from './styles.module.scss'
+import SocialLinks from '@site/src/components/SocialLinks'
+
+import { Icon, IconProps } from '@iconify/react'
+
+const variants: Variants = {
+  visible: i => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      damping: 25,
+      stiffness: 100,
+      duration: 0.3,
+      delay: i * 0.3,
+    },
+  }),
+  hidden: { opacity: 0, y: 30 },
+}
+
+function Logos() {
+  const { scrollYProgress } = useScroll()
+
+  // 往下滚动 元素向上移动
+  const y1 = useTransform(scrollYProgress, [0, 1], ['0%', '-500%'], {
+    clamp: false,
+  })
+
+  // 往下滚动 元素向下移动
+  const y2 = useTransform(scrollYProgress, [0, 1], ['0%', '500%'], {
+    clamp: false,
+  })
+
+  const logos: IconProps[] = [
+    {
+      icon: 'logos:github',
+      style: { left: '1%', top: '1%' },
+    },
+    {
+      icon: 'logos:c',
+      style: { left: '4%', top: '5%' },
+    },
+
+    {
+      icon: 'logos:clion',
+      style: { right: '2%', top: '11%' },
+    },
+    {
+      icon: 'logos:visual-studio-code',
+      style: { right: '8%', top: '14%' },
+    },
+
+    {
+      icon: 'logos:linux-tux',
+      style: { top: '5%', left: '52%' },
+    },
+    {
+      icon: 'logos:arm',
+      style: { top: '9%', left: '58%' },
+    },
+
+    {
+      icon: 'logos:markdown',
+      style: { top: '15%', left: '30%' },
+    },
+    {
+      icon: 'logos:qt',
+      style: { top: '20%', left: '38%' },
+    },
+
+    {
+      icon: 'logos:ubuntu',
+      style: { top: '30%', left: '70%' },
+    },
+
+    {
+      icon: 'logos:google',
+      style: { bottom: '25%', right: '5%' },
+    },
+    {
+      icon: 'logos:docusaurus',
+      style: { bottom: '1%', left: '1%' },
+    },
+  ]
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}
-        </h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            自我介绍
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}*/
-const svgList = [
-  {
-    title: 'github',
-    Svg: require('../../static/img/github.svg').default,
-    color: 'black',
-    link: 'https://github.com/DxIMT',
-  },
-  {
-    title: 'bilibili',
-    Svg: require('../../static/img/bilibili.svg').default,
-    link: 'https://space.bilibili.com/481621384?spm_id_from=333.1007.0.0',
-  },
-  {
-    title: 'wechat',
-    Svg: require('../../static/img/WeChat.svg').default,
-    color: '#2979ff',
-    link: 'https://postimg.cc/9rN4nx8X',
-  },
-  /*{
-    title: 'cloud_music',
-    Svg: require('../../static/img/cloud-music.svg').default,
-    color: '#2979ff',
-    link: 'https://music.163.com/#/user/home?id=557006813',
-  },*/
-]
+    <>
+      {logos.map((l, index) => {
+        const yValue = index % 2 === 0 ? y1 : y2
 
-const Svg = ({ Svg, color, title, link }) => {
-  return (
-    <a href={link} target='_blank'>
-      <Svg className={styles.svg} style={{ fill: color }} />
-    </a>
+        return (
+          <motion.div
+            className={styles.box}
+            initial={{ opacity: 0.01, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: Math.random() * 2 + 0.5,
+              delay: 0.5,
+            }}
+            style={{
+              ...l.style,
+              y: yValue,
+            }}
+          >
+            <Icon icon={l.icon}></Icon>
+          </motion.div>
+        )
+      })}
+    </>
   )
 }
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function Background() {
   return (
-    <div className={styles.myHeroContainer}>
-      <div className={styles.leftContainer}>
-        <h1 className={styles.leftContainer_h1}>
-        Hello！ <br />这里是🎥啵贝琴的小站💭
-        </h1>
-        <p className={styles.leftContainer_p}>
-        ➡ 这是属于啵贝琴的专属网页；记录自己学习的历程以及感悟。
-        <br />
-        <br />
-        ◻️ “困住你的到底是什么?”
-        <br />
-        ◻️ “是已有的认知。”
-        </p>
-        <div className={styles.buttonContainer}>
-          {/* <button className={styles.button}>
-            <a className={styles.hero_a} href='/'>
-              Click
-            </a>
-          </button>
-          <span className={styles.buttonLeftText}>
-            Get Started. <br /> 开启学习之旅.
-          </span> */}
-          
-          <div className={styles.svgContainer}>
-            {svgList.map((item, index) => {
-              return <Svg {...item} key={item.title} />
-            })}
-          </div>
-        </div>
-      </div>
-      <div className={styles.rightContainer}>
-        <img src={HeroImg} alt='HeroImg'/>
-      </div>
-    </div>
+    <>
+      <motion.div className={styles.background}>
+        <Logos />
+        <HeroMain />
+        <div className={styles.circle} />
+      </motion.div>
+    </>
   )
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+function Name() {
   return (
-    <Layout
-      title={`欢迎来到`}
+    <motion.div
+      className={styles.hero_text}
+      custom={1}
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      onMouseMove={e => {
+        e.currentTarget.style.setProperty('--x', `${e.clientX}px`)
+        e.currentTarget.style.setProperty('--y', `${e.clientY}px`)
+      }}
+    >
+      <Translate id="homepage.hero.greet">Hello👋! 我是</Translate>
+      <span
+        className={styles.name}
+        onMouseMove={e => {
+          const bounding = e.currentTarget.getBoundingClientRect()
+          e.currentTarget.style.setProperty('--positionX', `${bounding.x}px`)
+          e.currentTarget.style.setProperty('--positionY', `${bounding.y}px`)
+        }}
       >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+        <Translate id="homepage.hero.name">尚宇</Translate>
+      </span>
+      <span className={styles.wave}>👋</span>
+    </motion.div>
+  )
+}
+
+export default function Hero() {
+  return (
+    <motion.div className={styles.hero}>
+      <div className={styles.intro}>
+        <Name />
+        <motion.p
+          custom={2}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+        >
+          <Translate id="homepage.hero.text">
+            {`我在这里记录一些知识，以免遗忘，也希望对你有用，请随意浏览～`}
+          </Translate>
+        </motion.p>
+        <motion.div
+          custom={3}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+        >
+          <SocialLinks />
+        </motion.div>
+
+        <motion.div
+          className={styles.buttonGroup}
+          custom={4}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+        >
+          <div className={styles.outer}>
+            <div className={styles.gradient} />
+            <a className={styles.button} href={'./about'}>
+              <Translate id="hompage.hero.introduce">自我介绍</Translate>
+            </a>
+          </div>
+        </motion.div>
+      </div>
+      <Background />
+    </motion.div>
+  )
 }
