9a4e2201061d764c422879ee61dcfbfdc4aa0a06
hwchu <hwchu@zdmedtech.com>
2024-11-05 16:19:58 +0800

    first commit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,6 +50,7 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
+    <HomepageHeader />
     <main>
       <HomepageFeatures />
     </main>
